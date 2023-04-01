8a43ab264e7bf006dc69f04805d3c500af8e3169
ksorthiya <ksorthiya@horizontalintegration.com>
2023-04-01 14:52:46 +0530

    Service Component: Initial Commit.

---
+++ src/components/service/services.tsx
@@ -0,0 +1,25 @@
+const Services = () => {
+  return (
+    <section className="section service gray-bg">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-7 text-center">
+            <div className="section-title">
+              <h2>Award winning patient care</h2>
+              <div className="divider mx-auto my-4"></div>
+              <p>
+                Lets know moreel necessitatibus dolor asperiores illum possimus
+                sint voluptates incidunt molestias nostrum laudantium. Maiores
+                porro cumque quaerat.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="row"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
