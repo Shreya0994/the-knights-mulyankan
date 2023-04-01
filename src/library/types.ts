f618766207aecf076f813fa11757af5089bd37ad
Jatin Prajapati <jatin@jprajapati.in>
2023-04-01 12:05:51 +0530

    moved companyInformation data object to data folder

--- src/library/types.ts
+++ src/library/types.ts
@@ -35,30 +35,3 @@ export class CompanyInformation implements ICompanyInformation {
     this.socialLinks = socialLinks;
   }
 }
-
-const companyInformation = new CompanyInformation(
-  {
-    email: "support@gmail.com",
-    address: "Address Ta-134/A, New York, USA",
-    phoneNumber: "823-4565-13456",
-    timing: "Mon to Fri: 08:30 - 18:00",
-  },
-  "https://themewagon.github.io/novena/images/logo.png",
-  "Tempora dolorem voluptatum nam vero assumenda voluptate, facilis ad eos obcaecati tenetur veritatis eveniet distinctio possimus.",
-  [
-    {
-      href: "www.facebook.com",
-      iconClass: "icofont-facebook",
-    },
-    {
-      href: "www.twitter.com",
-      iconClass: "icofont-twitter",
-    },
-    {
-      href: "www.linkedin.com",
-      iconClass: "icofont-linkedin",
-    },
-  ]
-);
-
-export { companyInformation };
