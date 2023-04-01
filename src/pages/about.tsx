5d9acfd8ff5d64b2c91b5cfd99e3694746ee02bc
Ajay Gour <agour@horizontal.com>
2023-04-01 21:16:14 +0530

    About page and department page

---
+++ src/pages/about.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Cards from "@/components/cards/cards";
+import { CardsItems } from "@/data/cards";
+import PageBanner from "@/components/pageBanner/pagebanner";
+import ContentBlock from "@/components/contentblock/contentblock";
+import { Page } from "@/library/types";
+import Specialist from "@/components/specialists/specialists";
+import { Recommendation } from "@/data/testimonial";
+import Recommendations from "@/components/recommendation/recommendations";
+
+const About = () => {
+    
+    return(
+        <>
+            <PageBanner subHeading="About Us" heading="About Us" />
+            <ContentBlock
+                title="Award winning patient care"
+                description="Lets know moreel necessitatibus dolor asperiores illum possimus sint voluptates incidunt molestias nostrum laudantium. Maiores porro cumque quaerat."
+                image="https://themewagon.github.io/novena/images/about/sign.png"
+            />
+            <Cards cards={CardsItems} page={Page.About}/>
+            <Specialist />
+            <Recommendations title={Recommendation.title} items={Recommendation.items}/>
+        </>
+    )
+}
+export default About;
