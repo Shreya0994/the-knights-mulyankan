28b789dd3d9cdea57f766728b4194f0df9fe1e51
ksorthiya <ksorthiya@horizontalintegration.com>
2023-04-02 11:41:48 +0530

    Added Specialist Component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,19 +9,24 @@ import { ReccomendationsData } from "@/data/reccomendations";
 import Recommendations from "@/components/recommendation/recommendations";
 
 const About = () => {
-    
-    return(
-        <>
-            <PageBanner subHeading="About Us" heading="About Us" />
-            <ContentBlock
-                title="Award winning patient care"
-                description="Lets know moreel necessitatibus dolor asperiores illum possimus sint voluptates incidunt molestias nostrum laudantium. Maiores porro cumque quaerat."
-                image="https://themewagon.github.io/novena/images/about/sign.png"
-            />
-            <Cards cards={CardsItems} page={Page.About}/>
-            <Specialist />
-            <Recommendations title={ReccomendationsData.title} testimonials={ReccomendationsData.testimonials}/>
-        </>
-    )
-}
+  return (
+    <>
+      <PageBanner subHeading="About Us" heading="About Us" />
+      <ContentBlock
+        title="Award winning patient care"
+        description="Lets know moreel necessitatibus dolor asperiores illum possimus sint voluptates incidunt molestias nostrum laudantium. Maiores porro cumque quaerat."
+        image="https://themewagon.github.io/novena/images/about/sign.png"
+      />
+      <Cards cards={CardsItems} page={Page.About} />
+      <Specialist
+        heading="Meet Our Specialist"
+        description="Today’s users expect effortless experiences. Don’t let essential people and processes stay stuck in the past. Speed it up, skip the hassles"
+      />
+      <Recommendations
+        title={ReccomendationsData.title}
+        testimonials={ReccomendationsData.testimonials}
+      />
+    </>
+  );
+};
 export default About;
