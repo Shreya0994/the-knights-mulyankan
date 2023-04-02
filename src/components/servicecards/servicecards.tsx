7fd7ea10a50c9bc5c8349c199353e2637ea0a390
Shreya Jain <shreya.jain@horizontal.com>
2023-04-02 10:40:45 +0530

    Implement Service Card Component

---
+++ src/components/servicecards/servicecards.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { IServiceCardInformation } from "@/library/servicecard";
+import Image from "next/image";
+
+const ServiceCards = () => {
+
+    const [data, setData] = useState<IServiceCardInformation[] | null>(null);
+    useEffect(() => {
+        fetch("/api/servicecard-information")
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+                console.log("data is", data);
+            });
+    }, []);
+
+    return (
+        <>
+            <section className="section service-2">
+                <div className="container">
+                    <div className="row">
+                        {
+                            data && data.map((serviceitem, index) => (
+                                <div className="col-lg-4 col-md-6 col-sm-6" key={index}>
+                                    <div className="service-block mb-5">
+                                        {data && serviceitem.image && <Image src={serviceitem.image} alt="" className="img-fluid" key={index} width={320} height={230} />}
+                                        <div className="content">
+                                            <h4 className="mt-4 mb-2 title-color">{serviceitem?.title}</h4>
+                                            <p className="mb-4">{serviceitem?.description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default ServiceCards;
---
+++ src/data/servicecard-information.ts
@@ -0,0 +1,44 @@
+import { ServiceCardInformation } from "@/library/servicecard";
+
+const servicecardInformation: ServiceCardInformation[]=
+  [
+    {
+        id:1,
+        image:"https://themewagon.github.io/novena/images/service/service-1.jpg",
+        title: "Child care",
+        description: "Saepe nulla praesentium eaque omnis perferendis a doloremque.",
+    },
+    {
+        id:2,
+        image:"https://themewagon.github.io/novena/images/service/service-2.jpg",
+        title: "Personal Care",
+        description: "Saepe nulla praesentium eaque omnis perferendis a doloremque.",
+    },
+    {
+        id:3,
+        image:"https://themewagon.github.io/novena/images/service/service-3.jpg",
+        title: "CT scan",
+        description: "Saepe nulla praesentium eaque omnis perferendis a doloremque.",
+    },
+    {
+        id:4,
+        image:"https://themewagon.github.io/novena/images/service/service-4.jpg",
+        title: "Joint replacement",
+        description: "Saepe nulla praesentium eaque omnis perferendis a doloremque.",
+    },
+    {
+        id:5,
+        image:"https://themewagon.github.io/novena/images/service/service-6.jpg",
+        title: "Examination & Diagnosis",
+        description: "Saepe nulla praesentium eaque omnis perferendis a doloremque.",
+    },
+    {
+        id:6,
+        image:"https://themewagon.github.io/novena/images/service/service-8.jpg",
+        title: "Alzheimer's disease",
+        description: "Saepe nulla praesentium eaque omnis perferendis a doloremque.",
+    }
+  ]
+;
+
+export { servicecardInformation };
