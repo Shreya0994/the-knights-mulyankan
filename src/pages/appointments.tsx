3e43c2bd38cbfa9256c6dce55bc6a388c9483736
Ajay Gour <agour@horizontal.com>
2023-04-03 15:01:06 +0530

    table changes

--- src/pages/appointments.tsx
+++ src/pages/appointments.tsx
@@ -18,15 +18,18 @@ const Appointment = ( props : { allAppointments :  IAppointment[] } ) => {
 			<div className="container">
 				<br/>
 				<table className="table" style={{ width: '100%' }}>
-					<tr style={ { border: '1px solid #dddddd' }}>
-						<th style={ { border: '1px solid #dddddd' }}>Doctor</th>
-						<th style={ { border: '1px solid #dddddd' }}>Department</th>
-						<th style={ { border: '1px solid #dddddd' }}>Patient Name</th>
-						<th style={ { border: '1px solid #dddddd' }}>Patient Phone</th>
-						<th style={ { border: '1px solid #dddddd' }}>Message</th>
-						<th style={ { border: '1px solid #dddddd' }}>Appointment Date</th>
-						<th style={ { border: '1px solid #dddddd' }}>Appointment Time</th>
-					</tr>
+					<thead>
+						<tr style={ { border: '1px solid #dddddd' }}>
+							<th style={ { border: '1px solid #dddddd' }}>Doctor</th>
+							<th style={ { border: '1px solid #dddddd' }}>Department</th>
+							<th style={ { border: '1px solid #dddddd' }}>Patient Name</th>
+							<th style={ { border: '1px solid #dddddd' }}>Patient Phone</th>
+							<th style={ { border: '1px solid #dddddd' }}>Message</th>
+							<th style={ { border: '1px solid #dddddd' }}>Appointment Date</th>
+							<th style={ { border: '1px solid #dddddd' }}>Appointment Time</th>
+						</tr>
+					</thead>
+					<tbody>
 					{props.allAppointments && props.allAppointments.map((app, index)=>
 						<tr key={index} style={ { border: '1px solid #dddddd' }}>
 							<td style={ { border: '1px solid #dddddd' }}>{app.doctor}</td>
@@ -38,6 +41,7 @@ const Appointment = ( props : { allAppointments :  IAppointment[] } ) => {
 							<td style={ { border: '1px solid #dddddd' }}>{app.time}</td>
 						</tr>
 					)}
+					</tbody>
 				</table>
 
 			</div>
